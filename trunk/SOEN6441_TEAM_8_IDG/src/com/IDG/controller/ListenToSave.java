a55bb2d48ce70e446158bcb50d101b1df6b273b2
ajaykariappa <ajaykariappa@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-22 04:35:05 +0000

    Action Listener to save button .

---
+++ trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/ListenToSave.java
@@ -0,0 +1,32 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.PrintWriter;
+
+
+public class ListenToSave implements ActionListener {
+	wall buttons[][];
+	PrintWriter writer=null;
+
+	public ListenToSave(wall buttons[][]){
+		try{
+			writer= new PrintWriter("F://map.txt", "UTF-8");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		this.buttons=buttons;
+	}
+	public void actionPerformed(ActionEvent e) {
+
+		for (int i=0;i<buttons.length;i++){
+			for (int j=0;j<buttons[0].length;j++){
+				writer.print(buttons[i][j].getDirection());
+
+			}
+			writer.println();
+		}
+		writer.close();
+		
+	}
+	
+}
+
