25d76cba0feabc7e381e83bbcfd3ea009f73e37c
ajaykariappa <ajaykariappa@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-24 05:10:39 +0000

    Integrating ListenToSave Class with MapeditorView Class and fixing exit issues with JFrame .

--- SOEN6441_TEAM_8_IDG/src/com/IDG/controller/ListenToSave.java
+++ SOEN6441_TEAM_8_IDG/src/com/IDG/controller/ListenToSave.java
@@ -1,34 +1,68 @@
 package com.IDG.controller;
 
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.PrintWriter;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import com.IDG.utils.MapValidityHelper;
 
 
 public class ListenToSave implements ActionListener {
 	wall buttons[][];
 	PrintWriter writer=null;
+	JFrame frame=new JFrame();
+	JFrame p2;
 
-	public ListenToSave(wall buttons[][]){
+	public ListenToSave(wall buttons[][], JFrame p2){
 		try{
 			writer= new PrintWriter("F://map.txt", "UTF-8");
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		this.buttons=buttons;
+		this.p2=p2;
+
 	}
 	public void actionPerformed(ActionEvent e) {
-
+		String [][] gridMap=new String[buttons.length][buttons[0].length];
 		for (int i=0;i<buttons.length;i++){
 			for (int j=0;j<buttons[0].length;j++){
-				writer.print(buttons[i][j].getDirection());
+				if(buttons[i][j].isStart){
+					writer.print("S");
+					gridMap[i][j]="S";
+				}
+				else if(buttons[i][j].isEnd){
+					writer.print("E");
+					gridMap[i][j]="E";
+				}
+				else if(buttons[i][j].isPath){
+					writer.print("-");
+					gridMap[i][j]="-";
+				}
+				else{
+					writer.print("*");
+					gridMap[i][j]="*";
+				}	
 
 			}
 			writer.println();
 		}
-		writer.close();
-		
-	}
-	
-}
+		boolean isValid=MapValidityHelper.testMapValidity(gridMap);
+		if(!isValid){
+		}else{
+			writer.flush();
+			writer.close();
+			JOptionPane.showMessageDialog(frame,
+					"Map Saved Succesfully.");
+			p2.dispose();
 
+		}
+
+	}
+
+}
