3dd117907d3ed6a393438f0cbc8921c3da3f3134
arjunkarnwal@gmail.com <arjunkarnwal@gmail.com@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-24 03:45:20 +0000

    Refactoring for MapValidty class. JavaDocs added for missing function. Listeners classes added for Mouse Right Click

--- trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/Arena.java
+++ trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/Arena.java
@@ -1,4 +1,4 @@
-\/**
+/**
  * 
  */
 package com.IDG.controller;
--- trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/ListenToSave.java
+++ trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/ListenToSave.java
@@ -1,3 +1,5 @@
+package com.IDG.controller;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.PrintWriter;
