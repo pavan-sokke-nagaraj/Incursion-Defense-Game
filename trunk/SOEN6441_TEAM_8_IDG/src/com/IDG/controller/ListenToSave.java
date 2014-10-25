f9ae3194628c2060d0076c37b5e4f027634f177b
arjunkarnwal@gmail.com <arjunkarnwal@gmail.com@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-25 05:41:05 +0000

    Saving on to user specified directory

--- trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/ListenToSave.java
+++ trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/ListenToSave.java
@@ -3,27 +3,26 @@ package com.IDG.controller;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+
 import com.IDG.utils.MapValidityHelper;
 
 
 public class ListenToSave implements ActionListener {
 	wall buttons[][];
-	PrintWriter writer=null;
 	JFrame frame=new JFrame();
 	JFrame p2;
-
+	StringBuffer matrix=new StringBuffer();
 	public ListenToSave(wall buttons[][], JFrame p2){
-		try{
-			writer= new PrintWriter("F://map.txt", "UTF-8");
-		}catch(Exception e){
-			e.printStackTrace();
-		}
 		this.buttons=buttons;
 		this.p2=p2;
 
@@ -33,36 +32,53 @@ public class ListenToSave implements ActionListener {
 		for (int i=0;i<buttons.length;i++){
 			for (int j=0;j<buttons[0].length;j++){
 				if(buttons[i][j].isStart){
-					writer.print("S");
+					matrix.append("S");
 					gridMap[i][j]="S";
 				}
 				else if(buttons[i][j].isEnd){
-					writer.print("E");
+					matrix.append("E");
 					gridMap[i][j]="E";
 				}
 				else if(buttons[i][j].isPath){
-					writer.print("-");
+					matrix.append("-");
 					gridMap[i][j]="-";
 				}
 				else{
-					writer.print("*");
+					matrix.append("*");
 					gridMap[i][j]="*";
 				}	
 
 			}
-			writer.println();
+			matrix.append(System.getProperty("line.separator"));
 		}
 		boolean isValid=MapValidityHelper.testMapValidity(gridMap);
 		if(!isValid){
-		}else{
-			writer.flush();
-			writer.close();
+			//If validation fails then show the error message
+			matrix=new StringBuffer();
 			JOptionPane.showMessageDialog(frame,
-					"Map Saved Succesfully.");
+					"Error");
+					
+		}else{
+			//if validation is succesfull ask the user to choose the destination as where to save the map file
+			try{
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setDialogTitle("Select the directory to Save");   
+				JFrame parentFrame = new JFrame();
+				int userSelection = fileChooser.showSaveDialog(parentFrame);
+				if (userSelection == JFileChooser.APPROVE_OPTION) {
+					File fileToSave = fileChooser.getSelectedFile();
+					FileOutputStream out = new FileOutputStream(fileToSave.getPath()+".txt");
+					byte[] b = matrix.toString().getBytes(Charset.forName("UTF-8"));
+					out.write(b);
+					out.close();
+				}
+			}catch(Exception ef){
+				ef.printStackTrace();
+			}
+		
+			JOptionPane.showMessageDialog(frame,
+			"Map Saved Succesfully.");
 			p2.dispose();
-
 		}
-
 	}
-
 }
