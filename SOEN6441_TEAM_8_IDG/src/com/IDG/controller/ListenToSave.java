1555b1f5c101a4cc5efac395c6c9a18be21db986
ajaykariappa <ajaykariappa@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-25 18:31:16 +0000

    Updated the mapbuilder class with Validation Error window and updated the corresponding ListenToSave class

--- SOEN6441_TEAM_8_IDG/src/com/IDG/controller/ListenToSave.java
+++ SOEN6441_TEAM_8_IDG/src/com/IDG/controller/ListenToSave.java
@@ -3,15 +3,13 @@ package com.IDG.controller;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.PrintWriter;
-import java.nio.charset.Charset;
+import java.util.ArrayList;
 
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 import com.IDG.utils.MapValidityHelper;
@@ -19,66 +17,75 @@ import com.IDG.utils.MapValidityHelper;
 
 public class ListenToSave implements ActionListener {
 	wall buttons[][];
+	PrintWriter writer=null;
 	JFrame frame=new JFrame();
 	JFrame p2;
-	StringBuffer matrix=new StringBuffer();
-	public ListenToSave(wall buttons[][], JFrame p2){
+	ArrayList<String> errorList=null;
+	JTextArea tArea1;
+
+	public ListenToSave(wall buttons[][], JFrame p2,ArrayList<String> errorList, JTextArea tArea1){
+		try{
+			writer= new PrintWriter("F://map.txt", "UTF-8");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		this.tArea1=tArea1;
+		this.errorList=errorList;
 		this.buttons=buttons;
 		this.p2=p2;
 
 	}
 	public void actionPerformed(ActionEvent e) {
+
+		System.out.println(" Printed ");
 		String [][] gridMap=new String[buttons.length][buttons[0].length];
 		for (int i=0;i<buttons.length;i++){
 			for (int j=0;j<buttons[0].length;j++){
 				if(buttons[i][j].isStart){
-					matrix.append("S");
+					writer.print("S");
 					gridMap[i][j]="S";
 				}
 				else if(buttons[i][j].isEnd){
-					matrix.append("E");
+					writer.print("E");
 					gridMap[i][j]="E";
 				}
 				else if(buttons[i][j].isPath){
-					matrix.append("-");
+					writer.print("-");
 					gridMap[i][j]="-";
 				}
 				else{
-					matrix.append("*");
+					writer.print("*");
 					gridMap[i][j]="*";
 				}	
 
 			}
-			matrix.append(System.getProperty("line.separator"));
+			writer.println();
 		}
-		boolean isValid=MapValidityHelper.testMapValidity(gridMap);
-		if(!isValid){
-			//If validation fails then show the error message
-			matrix=new StringBuffer();
-			JOptionPane.showMessageDialog(frame,
-					"Error");
-					
-		}else{
-			//if validation is succesfull ask the user to choose the destination as where to save the map file
-			try{
-				JFileChooser fileChooser = new JFileChooser();
-				fileChooser.setDialogTitle("Select the directory to Save");   
-				JFrame parentFrame = new JFrame();
-				int userSelection = fileChooser.showSaveDialog(parentFrame);
-				if (userSelection == JFileChooser.APPROVE_OPTION) {
-					File fileToSave = fileChooser.getSelectedFile();
-					FileOutputStream out = new FileOutputStream(fileToSave.getPath()+".txt");
-					byte[] b = matrix.toString().getBytes(Charset.forName("UTF-8"));
-					out.write(b);
-					out.close();
+		errorList=new ArrayList();
+		boolean isValid=MapValidityHelper.testMapValidity(gridMap,errorList);
+		StringBuffer erroCode=new StringBuffer();
+		if(!isValid)
+		{
+			if (errorList.size()>0)
+			{
+				for(int i=0;i<errorList.size();i++)
+				{
+					erroCode.append(errorList.get(i));
+					erroCode.append(System.getProperty("line.separator"));
 				}
-			}catch(Exception ef){
-				ef.printStackTrace();
+				tArea1.setText(erroCode.toString());
 			}
-		
+
+
+		}else{
+			writer.flush();
+			writer.close();
 			JOptionPane.showMessageDialog(frame,
-			"Map Saved Succesfully.");
+					"Map Saved Succesfully.");
 			p2.dispose();
+
 		}
+
 	}
+
 }
