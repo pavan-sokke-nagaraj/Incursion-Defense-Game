735f380e4f3c9760b583b8fc5b016e05a7a5df60
ajaykariappa <ajaykariappa@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-25 18:32:30 +0000

    Updated mapbuilder class with Validation Error window.

--- trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/mapbuilder.java
+++ trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/mapbuilder.java
@@ -4,13 +4,24 @@ import javax.swing.*;
 
 import java.awt.event.*;
 import java.awt.*;
+import java.util.ArrayList;
 public class mapbuilder extends JFrame {
 	JPanel p=new JPanel();
-	JPanel p1=new JPanel();
+	JFrame p1=new JFrame ();
 	JFrame p2=new JFrame();
-	JTextField textField = new JTextField(20);
+	JPanel p3=new JPanel();
+	JPanel contentPane = new JPanel();
+	MapEditorView mapeditorview = new MapEditorView();
+    ArrayList<String> errorList = null;
+
+    JTextArea tArea1 = new JTextArea(15,30);
+    
+	//JTextField textField = new JTextField(20);
+	JMenuBar menuBar = new JMenuBar();
+	JMenuItem save = new JMenuItem("Save");
+	JLabel headerLabel;
 	wall buttons[][];
-	JButton save,reset,load;
+	
 	
 	/**
 	public static void main(String[] args)
@@ -20,45 +31,45 @@ public class mapbuilder extends JFrame {
 
 public mapbuilder(int gridRow, int gridcolm){
 	super("MAPBuilder V 1.0");
-	JLabel headerLabel,headerLabel1;
-	
-	headerLabel = new JLabel("<html>Welcome to the MAP Editor <br>Click the buttons to set a Path</html>");
-	
+	//contentPane.setLayout(new GridLayout(1, 2, 2, 2));
+	tArea1.setLineWrap(true);
+	tArea1.setText("Validation Status");
+	 JScrollPane scroller1 = new JScrollPane();
+	 scroller1.setViewportView(tArea1);
+	 contentPane.add(scroller1);
 	buttons=new wall[gridRow][gridcolm];
+	headerLabel = new JLabel("<html>Welcome to the MAP Editor <br>Click the buttons to build a path</html>");
 	p2.setSize(850,700);
-	p2.setResizable(true);
+	p2.setResizable(false);
 	p.setLayout(new GridLayout(gridRow,gridcolm));
-	int counter=0;
+	//p1.setLayout(new GridLayout(2,1));
 	for(int i=0;i<gridRow;i++){
 		for(int j=0;j<gridcolm;j++){
 			buttons[i][j]=new wall();
 			buttons[i][j].setPreferredSize(new Dimension(40, 40));
-			buttons[i][j].id=counter++;
-			
 			p.add(buttons[i][j]);	
 		}
 	}
-	p1.setLayout(new GridLayout(3,1));
-	save = new JButton("Save");
-	save.setPreferredSize(new Dimension(200,100));
-	save.addActionListener(new ListenToSave(buttons,p2));
-	p1.add(headerLabel);
-	p1.add(save);
-	p1.add(textField);
+	errorList = new ArrayList<String>();
+	save.addActionListener(new ListenToSave(buttons,p2,errorList,tArea1));
 	
 	
+	menuBar.add(save);
+	p3.add(headerLabel);
+	//p1.setContentPane(contentPane);
 	
-	//p1.add(reset = new JButton("Reset"));
-	//p1.add(load = new JButton("Load"));
+	
 	//load.addActionListener(new ListenToLoad());
-	p2.setLayout(new GridBagLayout());
-	p2.add(p);
-	p2.add(p1);
-    
+	p2.setLayout(new BorderLayout());
+	p2.add(p3,BorderLayout.NORTH);
+	p2.add(contentPane,BorderLayout.EAST);
+	p2.add(p,BorderLayout.CENTER);
+	
+	p2.setJMenuBar(menuBar);
 	//add(p2);
-	//pack();
 	p2.setLocationRelativeTo(null);
-	p2.setVisible(true);
+    p2.setVisible(true);
+	
 	
 
 }
