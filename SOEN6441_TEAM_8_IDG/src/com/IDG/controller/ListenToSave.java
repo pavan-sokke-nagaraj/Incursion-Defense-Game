5a38f0ce891d341287bb3eced956f145405a83af
arjunkarnwal@gmail.com <arjunkarnwal@gmail.com@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-26 05:10:58 +0000

    Arjunk: Map Load and save

--- SOEN6441_TEAM_8_IDG/src/com/IDG/controller/ListenToSave.java
+++ SOEN6441_TEAM_8_IDG/src/com/IDG/controller/ListenToSave.java
@@ -1,17 +1,27 @@
 package com.IDG.controller;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
+import com.IDG.controller.wall;
 import com.IDG.utils.MapValidityHelper;
 
 /** ListenToSave class is a action listener, waiting for
@@ -24,37 +34,21 @@ import com.IDG.utils.MapValidityHelper;
 
 public class ListenToSave implements ActionListener {
 	wall buttons[][];
-	PrintWriter writer=null;
 	JFrame frame=new JFrame();
 	JFrame p2;
 	ArrayList<String> errorList=null;
 	JTextArea tArea1;
+	JPanel gameMatrixPanel=null;
+	StringBuffer fileContent=new StringBuffer();
 
-/** Call the constructor setting initial values.
- * @param Area1 A set of string showing why the map is invalid.
- * @param buttons[][] Each contains the state of field where map is created.
- */
-	public ListenToSave(wall buttons[][], JFrame p2,ArrayList<String> errorList, JTextArea tArea1){
-		try{
-			writer= new PrintWriter("F://map.txt", "UTF-8");
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+	public ListenToSave(wall buttons[][], JFrame p2,ArrayList<String> errorList, JTextArea tArea1,JPanel p){
 		this.tArea1=tArea1;
 		this.errorList=errorList;
 		this.buttons=buttons;
 		this.p2=p2;
+		gameMatrixPanel=p;
 
 	}
-   
-  /** Perform action to transfer map edited to the .txt file
-    * to save.
-    * @param gridMap[][] Read buttons[][] state and transfer to .txt file
-    * <p>"S" means the start of the road. </p>
-    * <p>"E" means the end of the road. </p>
-    * <p>"-" means there exsits a road. </p>
-    * <p>"*" means scenery.</p>
-    */
 	public void actionPerformed(ActionEvent e) {
 
 		System.out.println(" Printed ");
@@ -62,24 +56,24 @@ public class ListenToSave implements ActionListener {
 		for (int i=0;i<buttons.length;i++){
 			for (int j=0;j<buttons[0].length;j++){
 				if(buttons[i][j].isStart){
-					writer.print("S");
+					fileContent.append("S");
 					gridMap[i][j]="S";
 				}
 				else if(buttons[i][j].isEnd){
-					writer.print("E");
+					fileContent.append("E");
 					gridMap[i][j]="E";
 				}
 				else if(buttons[i][j].isPath){
-					writer.print("-");
+					fileContent.append("-");
 					gridMap[i][j]="-";
 				}
 				else{
-					writer.print("*");
+					fileContent.append("*");
 					gridMap[i][j]="*";
 				}	
 
 			}
-			writer.println();
+			fileContent.append(System.getProperty("line.separator"));
 		}
 		errorList=new ArrayList();
 		boolean isValid=MapValidityHelper.testMapValidity(gridMap,errorList);
@@ -98,14 +92,65 @@ public class ListenToSave implements ActionListener {
 
 
 		}else{
-			writer.flush();
-			writer.close();
+			try{
+				//Genrating Random File Name
+				Random rn = new Random();
+				int range = 1000 - 0 + 1;
+				int fileName =  rn.nextInt(range) + 0;
+				
+				/***************************************************START MAP SAVE CODE***************************************************/
+				//Code to save file name to a header file
+				try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("Resource/CustomMaps/GameMatrix/GameMatrixHeader.txt", true)))) {
+					out.println("Map"+fileName+".txt");
+				}catch (IOException ed) {
+				}
+				//Creating a Map File 
+				File file =new File("Resource/CustomMaps/GameMatrix/"+"Map"+fileName+".txt");
+				if(!file.exists()){
+	    			file.createNewFile();
+	    		}else{
+	    			PrintWriter writer = new PrintWriter(file);
+	    			writer.print("");
+	    			writer.close();
+	    		}
+				try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file, true)))) {
+				    out.println(fileContent.toString());
+				}catch (IOException ed) {
+				}
+				fileContent=new StringBuffer();
+				/***************************************************END MAP SAVE CODE***************************************************/
+				
+				/***************************************************START SCREENSHOT CODE***************************************************/
+				//Code to save file name to a header file
+				try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("Resource/CustomMaps/ScreenShots/ScreenShotsHeader.txt", true)))) {
+					out.println("ScreenShot"+fileName+".png");
+				}catch (IOException ed) {
+				}
+				//Code to save screen shots
+				BufferedImage matrixImage=null;
+				matrixImage = new BufferedImage(gameMatrixPanel.getSize().width, gameMatrixPanel.getSize().height,BufferedImage.TYPE_INT_RGB);
+				//matrixImage=resize(matrixImage,100,100);
+				gameMatrixPanel.printAll (matrixImage.getGraphics());
+				ImageIO.write(matrixImage, "png", new File("Resource/CustomMaps/ScreenShots/"+"ScreenShot"+fileName+".png"));
+				/***************************************************END SCREENSHOT CODE***************************************************/
+			} catch (Exception ef) {
+				ef.printStackTrace();
+			}
 			JOptionPane.showMessageDialog(frame,
 					"Map Saved Succesfully.");
 			p2.dispose();
-
 		}
 
 	}
+	public static BufferedImage resize(BufferedImage img, int newW, int newH) { 
+	    Image tmp = img.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+	    BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+	    Graphics2D g2d = dimg.createGraphics();
+	    g2d.drawImage(tmp, 0, 0, null);
+	    g2d.dispose();
+
+	    return dimg;
+	} 
 
 }
--- SOEN6441_TEAM_8_IDG/src/com/IDG/controller/Load.java
+++ SOEN6441_TEAM_8_IDG/src/com/IDG/controller/Load.java
@@ -14,13 +14,21 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import com.IDG.controller.MapButton;
+import com.IDG.controller.wall;
 
 
 public class Load  extends JFrame  {
@@ -31,7 +39,7 @@ public class Load  extends JFrame  {
 	JPanel gameButtons=new JPanel();
 	JPanel align =new JPanel();
 	JFrame p2= new JFrame();
-	
+	String fileName1=null;
 	JButton save = new JButton("Save");
 	JButton edit = new JButton("Edit");
 	JButton game = new JButton("Start Game");
@@ -50,29 +58,42 @@ public class Load  extends JFrame  {
 		super("MAPBuilder V 2.0");
 		p2.setSize(850,700);
 		p2.setLocationRelativeTo(null);
-		//p2.setResizable(false);
-		
-		
-		//p2.setLayout(new GridBagLayout());
-		
-		//JFrame frame = new JFrame();
-        
-        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        //frame.setLocationRelativeTo(null);
-        
-      
 		dMap1.setPreferredSize(new Dimension(60, 60));
 		udMap1.setPreferredSize(new Dimension(60, 60));
 		
 		
-		mapGrid.setLayout(new GridLayout(20,20));
-		udMapGrid.setLayout(new GridLayout(4,4));
-		udMapGrid.add(udMap1);
+		/*************************************************************MY CODE*********************************************************************/
+		//Read ScreenShotHeaderFile
+		Scanner sc=null;
+		String line1=null;
+		try {
+			sc = new Scanner(new File("Resource/CustomMaps/ScreenShots/ScreenShotsHeader.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		ArrayList <MapButton>buttonList= new ArrayList <MapButton> ();
+		while(sc.hasNextLine()) {
+			line1=sc.nextLine().toString();
+			if(line1!=null&&line1!=""){
+				setLabelsForScreenShots(line1,buttonList);
+			}
+			
+		}if(buttonList.size()==0)
+			udMapGrid.setLayout(new GridLayout(1,1));
+			else
+		udMapGrid.setLayout(new GridLayout(buttonList.size(),buttonList.size()));
+		for (int i=0;i<buttonList.size();i++)
+		{
+			udMapGrid.add(buttonList.get(i));
+		}
+		
+		dMapGrid.setLayout(new GridLayout(4,4));
+		/*************************************************************MY CODE*********************************************************************/
+		
+		/*udMapGrid.add(udMap1);
 		udMapGrid.add(udMap2);
 		udMapGrid.add(udMap3);
-		udMapGrid.add(udMap4);
-		dMapGrid.setLayout(new GridLayout(4,4));
+		udMapGrid.add(udMap4);*/
 		dMapGrid.add(dMap1);
 		dMapGrid.add(dMap2);
 		dMapGrid.add(dMap3);
@@ -87,17 +108,65 @@ public class Load  extends JFrame  {
 		
 		
 		
-		JFileChooser choice = new JFileChooser();
-		int option = choice.showOpenDialog(this);
-		File mapFile=null;
-		if (option == JFileChooser.APPROVE_OPTION) {
-			try{
-				mapFile=choice.getSelectedFile();
-			}catch(Exception e){
-			}
+				p2.setLayout(new BorderLayout());
+		
+		
+		//p2.add(udMapGrid,BorderLayout.EAST);
+		//p2.add(dMapGrid,BorderLayout.EAST);
+		p2.add(align,BorderLayout.EAST);
+		p2.add(gameButtons,BorderLayout.SOUTH);
+		Component[] components = p2.getComponents();
+		System.out.println("Number of Comp="+components.length);
+		//add(p2);
+		
+		p2.setVisible(true);
+		
+		//p2.setLayout(new GridLayout(loadbuttons.length,loadbuttons[0].length));
+		
+		//frame.pack();
+		//frame.setVisible(true);
+		/*p2.repaint();
+		p2.setLayout(new GridLayout(10,10));
+		p2.setVisible(true);*/
+	}
+	public void setLabelsForScreenShots(String fileName,ArrayList <MapButton>buttonList ){
+		BufferedImage myPicture=null;
+		fileName1=fileName;
+		try {
+		//	myPicture = ImageIO.read(new File("Resource/CustomMaps/ScreenShots/"+fileName));
+			myPicture = ImageIO.read(new File("D://end.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		//Snapshot code
+		//Image scaledImage = myPicture.getScaledInstance(jPanel.getWidth(),jPanel.getHeight(),Image.SCALE_SMOOTH);
+		fileName1=fileName1.replaceAll("png", "txt");
+		fileName1=fileName1.replaceAll("ScreenShot", "Map");
+		System.out.println(fileName1);
+		MapButton picLabel = new MapButton(fileName1);
+		//picLabel.setIcon(new ImageIcon(myPicture));
+		//picLabel.setMaximumSize(new Dimension(10,10));
+		picLabel.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				MapButton file = (MapButton)e.getSource();
+				setGameMatrixOnPanel(new File("Resource/CustomMaps/GameMatrix/"+file.fileName));
+			}
+		});
+		buttonList.add(picLabel);
+	}
+	
+	public void setGameMatrixOnPanel(File mapFile){
+		System.out.println(mapFile.getName());
+		mapGrid.removeAll();
+		mapGrid.revalidate();
+		
+		mapGrid.setVisible(false);
+		
+		//p2.dispose();
 		List<String> lines=new ArrayList();
 		Scanner sc=null;
+		String line1=null;
 		try {
 			sc = new Scanner(mapFile);
 		} catch (FileNotFoundException e) {
@@ -105,7 +174,10 @@ public class Load  extends JFrame  {
 			e.printStackTrace();
 		}
 		while(sc.hasNextLine()) {
-			lines.add(sc.nextLine());
+			line1=sc.nextLine().toString().trim();
+			if(line1!=null&&!line1.isEmpty()){
+				lines.add(line1);
+			}
 		}
 
 		// to array
@@ -157,32 +229,21 @@ public class Load  extends JFrame  {
 				loadbuttons[i][j].setPreferredSize(new Dimension(40,40));
 				mapGrid.add(loadbuttons[i][j]);
 			}}
-		p2.setLayout(new BorderLayout());
-		p2.add(mapGrid,BorderLayout.CENTER);
-		
-		//p2.add(udMapGrid,BorderLayout.EAST);
-		//p2.add(dMapGrid,BorderLayout.EAST);
-		p2.add(align,BorderLayout.EAST);
-		p2.add(gameButtons,BorderLayout.SOUTH);
-		Component[] components = p2.getComponents();
-		System.out.println("Number of Comp="+components.length);
-		//add(p2);
 		for ( i=0;i<loadbuttons.length;i++){
 			for ( j=0;j<loadbuttons[0].length;j++){
 				if(!loadbuttons[i][j].isPath)
 				loadbuttons[i][j].setEnabled(false);
 				
+				
 			}
 		}
+		mapGrid.setVisible(true);
 		mapGrid.setLayout(new GridLayout(loadbuttons.length,loadbuttons[0].length));
+		System.out.println("Number="+mapGrid.getComponentCount());
+		p2.add(mapGrid,BorderLayout.CENTER);
 		p2.setVisible(true);
-		//p2.setLayout(new GridLayout(loadbuttons.length,loadbuttons[0].length));
 		
-		//frame.pack();
-		//frame.setVisible(true);
-		/*p2.repaint();
-		p2.setLayout(new GridLayout(10,10));
-		p2.setVisible(true);*/
+
 	}
 
 }
