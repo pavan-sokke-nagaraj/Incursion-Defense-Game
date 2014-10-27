0c60519bd47c5a846b65f531a1a7cb63b73513e8
ajaykariappa <ajaykariappa@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-27 15:45:37 +0000

    Restructuring

---
+++ trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/utils/PopUpDemo.java
@@ -0,0 +1,38 @@
+package com.IDG.utils;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+import com.IDG.mapBuilder.MapBuilderModel;
+
+
+public class PopUpDemo extends JPopupMenu {
+    JMenuItem wall;
+    JMenuItem startPoint;
+    JMenuItem endPoint;
+    MapBuilderModel button=null;
+	public PopUpDemo(MapBuilderModel but){
+    	this.button=but;
+    	startPoint = new JMenuItem("Start Point");
+    	startPoint.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+            	button.isStart=true;
+            	System.out.println("Start Point");
+            	button.setIcon(button.getStart());
+            }
+        });
+    	endPoint = new JMenuItem("End Point");
+    	endPoint.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+            	button.isEnd=true;
+            	System.out.println("End Point");
+            	button.setIcon(button.getEnd());
+            }
+        });
+    	add(startPoint);
+        add(endPoint);
+    }
+
+}
