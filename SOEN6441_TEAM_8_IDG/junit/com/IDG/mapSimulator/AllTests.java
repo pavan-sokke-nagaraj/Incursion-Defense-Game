c847c27ee2dda2ef2f3857cadfd62ed2623d13b2
Guanwang.felia@gmail.com <Guanwang.felia@gmail.com@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-27 15:28:54 +0000

    change

--- SOEN6441_TEAM_8_IDG/junit/com/IDG/mapSimulator/AllTests.java
+++ SOEN6441_TEAM_8_IDG/junit/com/IDG/mapSimulator/AllTests.java
@@ -4,29 +4,4 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/**
- * The class <code>TestAll</code> builds a suite that can be used to run all
- * of the tests within its package as well as within any subpackages of its
- * package.
- *
- * @version $Revision: 1.0 
- */
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-	RoomTest.class,
-	ArsenalTest.class,
-	TowerTest.class,
-})
-public class TestAll {
-
-	/**
-	 * Launch the test.
-	 *
-	 * @param args the command line arguments
-	 *
-	 *
-	 */
-	public static void main(String[] args) {
-		JUnitCore.runClasses(new Class[] { TestAll.class });
-	}
 }
