1aa77666467e9cf4a77cd2791aae48f98395d84b
Guanwang.felia@gmail.com <Guanwang.felia@gmail.com@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-27 14:27:25 +0000

    test

--- SOEN6441_TEAM_8_IDG/junit/com/IDG/Controller/GameControllerTest.java
+++ SOEN6441_TEAM_8_IDG/junit/com/IDG/Controller/GameControllerTest.java
@@ -1,14 +1,67 @@
-package junit.com.IDG.Controller;
+package com.IDG.controller;
 
+import org.junit.*;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
-
+/**
+ * The class <code>GameControllerTest</code> contains tests for the class <code>{@link GameController}</code>.
+ *
+ * @version Revision: 1.0 
+ */
 public class GameControllerTest {
-
+	/**
+	 * Run the void main(String[]) method test.
+	 *
+	 * @throws Exception
+	 *
+	*
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMain_1()
+		throws Exception {
+		String[] args = new String[] {};
+
+		GameController.main(args);
+
+		// add additional test code here
 	}
 
+	/**
+	 * Perform pre-test initialization.
+	 *
+	 * @throws Exception
+	 *         if the initialization fails for some reason
+	 *
+	*
+	 */
+	@Before
+	public void setUp()
+		throws Exception {
+		// add additional set up code here
+	}
+
+	/**
+	 * Perform post-test clean-up.
+	 *
+	 * @throws Exception
+	 *         if the clean-up fails for some reason
+	 *
+	*
+	 */
+	@After
+	public void tearDown()
+		throws Exception {
+		// Add additional tear down code here
+	}
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 *
+	*
+	 */
+	public static void main(String[] args) {
+		new org.junit.runner.JUnitCore().run(GameControllerTest.class);
+	}
 }
