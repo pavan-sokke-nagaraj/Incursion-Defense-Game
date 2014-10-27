1f0723c4e06e1599788323fd3050aeb9c5a7f9e7
reem.baiazid <reem.baiazid@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-27 14:18:30 +0000

    test

---
+++ trunk/SOEN6441_TEAM_8_IDG/junit/com/IDG/Controller/GameFileManagerTest.java
@@ -0,0 +1,14 @@
+package junit.com.IDG.Controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameFileManagerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ trunk/SOEN6441_TEAM_8_IDG/junit/com/IDG/Controller/LayoutManagerTest.java
@@ -0,0 +1,14 @@
+package junit.com.IDG.Controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LayoutManagerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
