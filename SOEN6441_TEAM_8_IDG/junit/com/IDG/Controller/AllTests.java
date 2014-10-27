37491fab32ffd1a4cc7d998e1012879e49518fc6
reem.baiazid <reem.baiazid@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-10-27 14:18:30 +0000

    test

---
+++ SOEN6441_TEAM_8_IDG/junit/com/IDG/Controller/AllTests.java
@@ -0,0 +1,17 @@
+package junit.com.IDG.Controller;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AllTests extends TestCase {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		//$JUnit-BEGIN$
+
+		//$JUnit-END$
+		return suite;
+	}
+
+}
