f6ad7feed6c9b9b739b4e4d3cd0e0e5def5af770
pavansn8 <pavansn8@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-11-12 15:33:02 +0000

    Code changes:
    Added Singleton pattern for Game
    Colosseum.java, Game.java, GameFileManagaer.java, LayoutManager.java
    
    .classpath - change in source path and build path.

--- trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/GameFileManager.java
+++ trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/GameFileManager.java
@@ -43,7 +43,7 @@ public class GameFileManager {
 		//JFileChooser fileChooser = new JFileChooser();
 		char a2Array[][] = new char[20][20];
 		Block block[][] = new Block[20][20];
-		Game game = new Game();
+		Game game = Game.getInstance();
 		//int openValue = fileChooser.showOpenDialog(game);
 		if (file!=null) {
 			//String filename = fileChooser.getSelectedFile().getName();
@@ -108,7 +108,7 @@ public class GameFileManager {
 	 */
 	public static void saveGameFile(Block[][] block) {
 		JFileChooser fileChooser = new JFileChooser();
-		Game game = new Game();
+		Game game = Game.getInstance();
 		int saveValue = fileChooser.showSaveDialog(game);
 		if (saveValue == JFileChooser.APPROVE_OPTION) {
 			String filename = fileChooser.getSelectedFile().getName();
