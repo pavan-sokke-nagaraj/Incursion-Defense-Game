cf97f34a874622ce5cbd70927dcb076fe2c52cf4
arjunkarnwal <arjunkarnwal@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-11-16 07:37:55 +0000

    Adding tower attacking startergies

--- trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/mapSimulator/Arsenal.java
+++ trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/mapSimulator/Arsenal.java
@@ -48,7 +48,7 @@ public class Arsenal {
 	/**
 	 * variable to hold the starting x position to draw tower information
 	 */
-	public int box2Xpos = 750;
+	public int box2Xpos = 710;
 	/**
 	 * variable to hold the starting y position to draw tower information
 	 */
@@ -121,6 +121,17 @@ public class Arsenal {
 
 	public Rectangle waveButton;
 
+	public Rectangle weakTargetStratergy;
+	public Rectangle strongTargetStratergy;
+	public Rectangle nearTargetStratergy;
+	public Rectangle farTargetStratergy;
+	public boolean isStrtergySelect = false;
+	public int selectedStratergy;
+	public static int weakTarget = 1;
+	public static int strongTarget = 2;
+	public static int farTarget = 3;
+	public static int nearTarget = 4;
+
 	/**
 	 * class constructor to initialize tower data, health data, money data
 	 * Upgrade: Build 2-> serialize he objects and retrieve data from xml files
@@ -132,14 +143,16 @@ public class Arsenal {
 			towers[i] = new Rectangle(box1Xpos + (gap * i) + (towerWidth * i),
 					box1Ypos, towerWidth, towerWidth);
 			if (i == 0) {
-				towerBlocks[i] = new Tower('G', 5, 1, 1, 10, "Single", 5, 5,1,5);
+				towerBlocks[i] = new Tower('G', 5, 1, 1, 10, "Single", 5, 5, 1,
+						5);
 			} else if (i == 1) {
-				towerBlocks[i] = new Tower('R', 10, 1, 2, 20, "Single", 5, 15,1,10);
+				towerBlocks[i] = new Tower('R', 10, 1, 2, 20, "Single", 5, 15,
+						1, 10);
 			} else if (i == 2) {
 				towerBlocks[i] = new Tower('B', 15, 1, 4, 20, "Multiple", 10,
-						20,1,20);
+						20, 1, 20);
 			} else if (i == 3) {
-				towerBlocks[i] = new Tower('D', 0, 0, 0, 0, "", 0, 0,1,10);
+				towerBlocks[i] = new Tower('D', 0, 0, 0, 0, "", 0, 0, 1, 10);
 			}
 		}
 
@@ -150,6 +163,16 @@ public class Arsenal {
 		upgradeButton = new Rectangle(box2Xpos + 45, box2Ypos + 145, 160, 40);
 		sellButton = new Rectangle(box2Xpos + 45, box2Ypos + 125, 160, 20);
 		waveButton = new Rectangle(box2Xpos + 45, 565 + 125 - 100, 160, 60);
+
+		// box2Xpos+280, box2Ypos, 200, 200
+		weakTargetStratergy = new Rectangle(box2Xpos + 300, box2Ypos + 30, 160,
+				30);
+		strongTargetStratergy = new Rectangle(box2Xpos + 300, box2Ypos + 70,
+				160, 30);
+		nearTargetStratergy = new Rectangle(box2Xpos + 300, box2Ypos + 110,
+				160, 30);
+		farTargetStratergy = new Rectangle(box2Xpos + 300, box2Ypos + 150, 160,
+				30);
 	}
 
 	/**
@@ -213,12 +236,12 @@ public class Arsenal {
 		graphic.drawImage(image, box1Xpos + 110, box1Ypos + towerWidth + 45,
 				towerWidth, towerWidth, null);
 
-		//		draw waveButton
-		graphic.fillRect(waveButton.x, waveButton.y, waveButton.width, waveButton.height);
+		// draw waveButton
+		graphic.fillRect(waveButton.x, waveButton.y, waveButton.width,
+				waveButton.height);
 		graphic.setColor(Color.RED);
 		graphic.setFont(new Font("Courier New", Font.BOLD, 20));
-		graphic.drawString("START WAVE", waveButton.x + 20, waveButton.y + 20 );
-
+		graphic.drawString("START WAVE", waveButton.x + 20, waveButton.y + 20);
 
 		drawMoney(graphic);
 
@@ -260,6 +283,7 @@ public class Arsenal {
 			int circleX = MapSimulatorView.room.block[mapTowerXpos][mapTowerYpos].x;
 			int circleY = MapSimulatorView.room.block[mapTowerXpos][mapTowerYpos].y;
 			drawTowerRange(graphic, circleX, circleY, tower.range);
+			drawStratergyWindow(graphic);
 		}
 		if (upgradeConfirm) {
 			drawUpgradeTowerInfo(graphic);
@@ -272,7 +296,18 @@ public class Arsenal {
 			graphic.fillRect(box2Xpos, box2Ypos, 250, 200);
 		}
 
-		//		System.out.println("Moveenemey:\t" + MapSimulatorView.moveEnemy);
+		// System.out.println("Moveenemey:\t" + MapSimulatorView.moveEnemy);
+		if (isStrtergySelect) {
+			applyStratergy(mapTowerXpos, mapTowerYpos);
+		}
+	}
+
+	private void applyStratergy(int mapTowerXpos2, int mapTowerYpos2) {
+		Tower tower = GameFileManager
+				.getTowerObject(mapTowerXpos, mapTowerYpos);
+		tower.attackStrategy = selectedStratergy;
+		System.out.println(selectedStratergy);
+		GameFileManager.saveTowerObject(tower, mapTowerXpos, mapTowerYpos);
 	}
 
 	/**
@@ -314,7 +349,7 @@ public class Arsenal {
 			mapTowerYpos = -1;
 			tower.drawTowerInformation(graphic);
 			GameFileManager
-			.deleteTowerObject(tower, mapTowerXpos, mapTowerYpos);
+					.deleteTowerObject(tower, mapTowerXpos, mapTowerYpos);
 		}
 	}
 
@@ -325,8 +360,9 @@ public class Arsenal {
 	 *            Graphic variable to draw the Components
 	 */
 	private void drawUpgradeTowerInfo(Graphics graphic) {
-		//		System.out.println("While drawUpgradeTowerInfo" + mapTowerXpos + "\t\t"
-		//				+ mapTowerYpos);
+		// System.out.println("While drawUpgradeTowerInfo" + mapTowerXpos +
+		// "\t\t"
+		// + mapTowerYpos);
 		Tower tower = GameFileManager
 				.getTowerObject(mapTowerXpos, mapTowerYpos);
 		if (MapSimulatorView.power >= tower.costToUpgrade) {
@@ -397,20 +433,42 @@ public class Arsenal {
 			} else if (sellButton.contains(MapSimulatorView.mse)) {
 				sellConfirm = true;
 				upgradeConfirm = false;
-			}else if(waveButton.contains(MapSimulatorView.mse)){
+			} else if (nearTargetStratergy.contains(MapSimulatorView.mse)) {
+				isStrtergySelect = true;
+				selectedStratergy = nearTarget;
+			} else if (farTargetStratergy.contains(MapSimulatorView.mse)) {
+				isStrtergySelect = true;
+				selectedStratergy = farTarget;
+			} else if (strongTargetStratergy.contains(MapSimulatorView.mse)) {
+				isStrtergySelect = true;
+				selectedStratergy = strongTarget;
+			} else if (weakTargetStratergy.contains(MapSimulatorView.mse)) {
+				isStrtergySelect = true;
+				selectedStratergy = weakTarget;
+			} else if (waveButton.contains(MapSimulatorView.mse)) {
 
-				if(MapSimulatorView.level<10){
+				if (MapSimulatorView.level < 10) {
 
-					MapSimulatorView.moveEnemy = true ;
+					MapSimulatorView.moveEnemy = true;
 					MapSimulatorView.level++;
-					if (MapSimulatorView.gridRow != 0 || MapSimulatorView.gridColumn != 0) {
+					if (MapSimulatorView.gridRow != 0
+							|| MapSimulatorView.gridColumn != 0) {
 						MapSimulatorView.enemyPath = EnemyPath.copyPath();
-						for (int i = 0; i < (MapSimulatorView.level * 10)/2; i++) {
-							MapSimulatorView.enemiesOnMap.add(MapSimulatorView.enemyFactory.getEnemyfromFactory(MapSimulatorView.enemyType,i,MapSimulatorView.enemyPath, i * 20,MapSimulatorView.level*100,MapSimulatorView.level*100,MapSimulatorView.level*5));
+						for (int i = 0; i < (MapSimulatorView.level * 10) / 2; i++) {
+							MapSimulatorView.enemiesOnMap
+									.add(MapSimulatorView.enemyFactory
+											.getEnemyfromFactory(
+													MapSimulatorView.enemyType,
+													i,
+													MapSimulatorView.enemyPath,
+													i * 20,
+													MapSimulatorView.level * 100,
+													MapSimulatorView.level * 100,
+													MapSimulatorView.level * 5));
 						}
 					}
-				}else{
-					//TODO  
+				} else {
+					// TODO
 				}
 			} else {
 				for (int i = 0; i < MapSimulatorView.room.mapRow; i++) {
@@ -421,7 +479,7 @@ public class Arsenal {
 							mapTowerXpos = i;
 							mapTowerYpos = j;
 
-							//							System.out.println("I click");
+							// System.out.println("I click");
 							if (createId == 'G' || createId == 'R'
 									|| createId == 'B') {
 								selectMapTower = true;
@@ -445,6 +503,36 @@ public class Arsenal {
 		circleY = circleY + (towerWidth / 2) - radius;
 		graphic.setColor(Color.RED);
 		graphic.drawOval(circleX, circleY, 2 * radius, 2 * radius);
+	}
+
+	private void drawStratergyWindow(Graphics graphic) {
+		graphic.clearRect(box2Xpos + 280, box2Ypos, 200, 200);
+		graphic.setColor(Color.WHITE);
+		graphic.fillRect(box2Xpos + 280, box2Ypos, 200, 200);
+		graphic.setFont(new Font("Courier New", Font.BOLD, 20));
+		graphic.setColor(Color.BLACK);
+		graphic.drawString("SELECT STRATERGY", box2Xpos + 280, box2Ypos + 20);
+
+		graphic.setColor(new Color(255, 155, 0));
+		graphic.fillRect(weakTargetStratergy.x, weakTargetStratergy.y,
+				weakTargetStratergy.width, weakTargetStratergy.height);
+		graphic.fillRect(strongTargetStratergy.x, strongTargetStratergy.y,
+				strongTargetStratergy.width, strongTargetStratergy.height);
+		graphic.fillRect(nearTargetStratergy.x, nearTargetStratergy.y,
+				nearTargetStratergy.width, nearTargetStratergy.height);
+		graphic.fillRect(farTargetStratergy.x, farTargetStratergy.y,
+				farTargetStratergy.width, farTargetStratergy.height);
+
+		graphic.setColor(Color.RED);
+		graphic.drawString(" WEAK TARGET", weakTargetStratergy.x,
+				weakTargetStratergy.y + 20);
+		graphic.drawString("STRONG TARGET", strongTargetStratergy.x,
+				strongTargetStratergy.y + 20);
+		graphic.drawString(" NEAR TARGET", nearTargetStratergy.x,
+				nearTargetStratergy.y + 20);
+		graphic.drawString(" FAR TARGET", farTargetStratergy.x,
+				farTargetStratergy.y + 20);
+
 	}
 
 }
