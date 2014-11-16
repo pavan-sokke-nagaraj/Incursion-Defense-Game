6f7b66f472745a675fdb310bcdbaf5085c3b0001
ajaykariappa <ajaykariappa@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-11-16 01:41:06 +0000

    Enemy movement and creation of two enemy types with specific charecterstics.

--- SOEN6441_TEAM_8_IDG/src/com/IDG/enemyFactory/EnemyFactory.java
+++ SOEN6441_TEAM_8_IDG/src/com/IDG/enemyFactory/EnemyFactory.java
@@ -1,53 +1,26 @@
 package com.IDG.enemyFactory;
 
-import java.awt.BorderLayout;
-import java.awt.event.*;
+import java.awt.Point;
+import java.util.LinkedList;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
-import com.IDG.mapBuilder.MapBuilderController;
-import com.IDG.mapSimulator.MapSimulatorView;
+public class EnemyFactory {
 
-public class EnemyFactory{
-	
-	public EnemyFactory() {
-		if(!MapBuilderController.enemyCanBeCreated){
-			
-			JFrame popAlert = new JFrame();
-			JButton button = new JButton("OK");
-			JLabel label1 = new JLabel("             First create/load a map");
-			popAlert.add(button, BorderLayout.SOUTH);
-			popAlert.add(label1, BorderLayout.CENTER);
-			button.addActionListener(new ActionListener(){
-
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					popAlert.dispose();
-				}
-				
-			});
-			popAlert.setSize(300, 200);
-			popAlert.setVisible(true);
-			popAlert.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		}else{
-			StartEnemy abc = null;
-			abc= new StartEnemy();
-			
-			
-		}
-	}
-	
-	public EnemyType getEnemy(int enemyID){
-		
-		if(enemyID == 0)
-			return new smallEnemies();
-		else if(enemyID == 1)
-			return new bossEnemy();
-		else
-			return null;
-	}
-
+public EnemyType getEnemyfromFactory(String enemyType,int enemyId,LinkedList<Point> enemyPath, int movePoint,int actualHealth,int currentHealth,int value)
+{
+	 if(enemyType == null)
+      	return new SmallEnemy();
+	 else if(enemyType.equalsIgnoreCase("normalenemy")){
+		 	//SmallEnemy [] array= new SmallEnemy[5];
+		 	 return new SmallEnemy(enemyId,enemyPath,movePoint,actualHealth,currentHealth,value);
+	 }
+	 else if(enemyType.equalsIgnoreCase("bossenemy")){
+		 //bossEnemy [] array= new bossEnemy[5];
+		 return new bossEnemy(enemyId,enemyPath,movePoint,actualHealth,currentHealth,value);
+	 }
+           
+	 return null;
+         
+}
 
 }
