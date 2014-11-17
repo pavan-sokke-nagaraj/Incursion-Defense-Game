19ee5fef5eb7b94be578027ca793f6904ff5c703
ajaykariappa <ajaykariappa@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-11-17 16:15:30 +0000

    Javadoc update

--- SOEN6441_TEAM_8_IDG/src/com/IDG/enemyFactory/EnemyType.java
+++ SOEN6441_TEAM_8_IDG/src/com/IDG/enemyFactory/EnemyType.java
@@ -32,28 +32,55 @@ public interface EnemyType {
 	 */
 	public void drawHealthBar(Graphics g);
 	
+	
+	/**
+	 * getter method for X
+	 */
 	public int Xvalue();
-
+	/**
+	 * getter method for Y
+	 */
 	public int Yvalue();
 	
-	//Below return methods are getter and setter functions
+	/**
+	 * getter method for actualhealth
+	 */
 	public int getActualHealth();
-	
+	/**
+	 * getter method for currenthealth
+	 */
 	public int getCurrentHealth();
-	
+	/**
+	 * Setter method for actualhealth
+	 */
 	public void setActualHealth(int actualHealth); 
-	
+	/**
+	 * setter method for currenthealth
+	 */
 	public void setCurrentHealth(int currentHealth);
-	
+	/**
+	 * getter method for EnemyId
+	 */
 	public int getEnemyId();
-	
+	/**
+	 * getter method for CurrentSpeed()
+	 */
 	public int getEnemyCurrentSpeed();
+	/**
+	 * getter method for isSpeedSlow
+	 */
 	public boolean isSpeedSlow();
-	
+	/**
+	 * getter method for EnemyId
+	 */
 	public void setSpeedSlow(boolean isSpeedSlow);
+	/**
+	 * getter method for EnemyCurrentSpeed
+	 */
 	public void setEnemyCurrentSpeed(int enemyCurrentSpeed);
-
-
+	/**
+	 * setter method for EnemyId
+	 */
 	public void setEnemyId(int enemyId);
 	
 }
