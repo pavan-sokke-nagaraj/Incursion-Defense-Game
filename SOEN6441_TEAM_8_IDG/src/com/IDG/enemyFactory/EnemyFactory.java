0d763af8a53c4d3a236f3f63a1270d1fa3eae18b
arjunkarnwal@gmail.com <arjunkarnwal@gmail.com@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-11-17 09:01:15 +0000

    Removal of Unused Code

--- SOEN6441_TEAM_8_IDG/src/com/IDG/enemyFactory/EnemyFactory.java
+++ SOEN6441_TEAM_8_IDG/src/com/IDG/enemyFactory/EnemyFactory.java
@@ -5,9 +5,25 @@ import java.util.LinkedList;
 
 import com.IDG.mapSimulator.MapSimulatorView;
 
-
+/**
+ * This is factory class to create different kind of enemies
+ * @author Arjun
+ *
+ */
 public class EnemyFactory {
-
+/**
+ * 
+ * @param enemyType Type of enemy 
+ * @param enemyId Id of enemy
+ * @param enemyPath Path of enemy
+ * @param movePoint Path of enemy
+ * @param actualHealth Actual Health of enemy
+ * @param currentHealth Current Health of enemy
+ * @param value Value of enemy
+ * @param enemyActualSpeed Actual Speed of enemy
+ * @param enemyCurrentSpeed Current Speed of enemy
+ * @return enemy
+ */
 public EnemyType getEnemyfromFactory(String enemyType,int enemyId,LinkedList<Point> enemyPath, int movePoint,int actualHealth,int currentHealth,int value,int enemyActualSpeed,int enemyCurrentSpeed)
 {
 	 if(enemyType == null)
