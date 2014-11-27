9eb84a2682d7aec29c3b661e34525a9bfa27be19
arjunkarnwal@gmail.com <arjunkarnwal@gmail.com@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-11-27 04:31:47 +0000

    methods added to save and load game state..

--- trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/GameFileManager.java
+++ trunk/SOEN6441_TEAM_8_IDG/src/com/IDG/controller/GameFileManager.java
@@ -3,6 +3,10 @@
  */
 package com.IDG.controller;
 
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
@@ -17,6 +21,7 @@ import java.util.Scanner;
 import javax.swing.JFileChooser;
 
 import com.IDG.mapSimulator.Block;
+import com.IDG.mapSimulator.MapSimulatorView;
 import com.IDG.mapSimulator.Tower;
 
 /**
@@ -30,7 +35,10 @@ public class GameFileManager {
 	/**
 	 * default directory to save the game file
 	 */
-	static String directory = "GameSave";
+	static String gameDirectory = "GameSave//";
+	static String tempDirectory = "temp//";
+	static String gameExtension = ".IDG";
+	static String towerExtension = ".TOWER";
 
 	/**
 	 * function to load a saved game
@@ -43,7 +51,7 @@ public class GameFileManager {
 		char a2Array[][] = new char[20][20];
 		Block block[][] = new Block[20][20];
 		Game game = Game.getInstance();
-		if (file!=null) {
+		if (file != null) {
 			int tempRow = 0;
 			int tempCol = 0;
 			int row = 0;
@@ -53,7 +61,7 @@ public class GameFileManager {
 				Scanner input = new Scanner(file);
 				while (input.hasNextLine()) {
 					String line = input.nextLine();
-					if(line!=null&&!line.isEmpty()){
+					if (line != null && !line.isEmpty()) {
 						tempCol = line.length();
 						++tempRow;
 					}
@@ -88,10 +96,9 @@ public class GameFileManager {
 				}
 			}
 			return a2Array;
-		}else
+		} else
 			return null;
-			
-		
+
 	}
 
 	/**
@@ -138,8 +145,11 @@ public class GameFileManager {
 	 */
 	public static void saveTowerObject(Tower towerBlocks, int x, int y) {
 		try {
-			FileOutputStream fos = new FileOutputStream("GameSave\\GameSave"
-					+ x + y);
+
+			String fileName = tempDirectory
+					+ Game.getInstance().getPlayingMapName() + x + y
+					+ towerExtension;
+			FileOutputStream fos = new FileOutputStream(fileName);
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 			oos.writeObject(towerBlocks); // write Tower object
 			oos.close();
@@ -160,13 +170,15 @@ public class GameFileManager {
 	public static Tower getTowerObject(int x, int y) {
 		Tower tower = new Tower();
 		try {
-			FileInputStream fos = new FileInputStream("GameSave\\GameSave" + x
-					+ y);
+			String fileName = tempDirectory
+					+ Game.getInstance().getPlayingMapName() + x + y
+					+ towerExtension;
+			FileInputStream fos = new FileInputStream(fileName);
 			ObjectInputStream oip = new ObjectInputStream(fos);
 			tower = (Tower) oip.readObject();
 			oip.close();
 		} catch (Exception ex) {
-//			ex.printStackTrace();
+			// ex.printStackTrace();
 		}
 		return tower;
 	}
@@ -184,5 +196,93 @@ public class GameFileManager {
 	public static void deleteTowerObject(Tower tower, int x, int y) {
 		File file = new File("GameSave\\GameSave" + x + y);
 		file.delete();
+	}
+
+	public void saveGameState(MapSimulatorView mapSimulatorView) {
+
+		GameStateVO gameStateVO = new GameStateVO();
+		gameStateVO.setGridRow(mapSimulatorView.gridRow);
+		gameStateVO.setGridColumn(mapSimulatorView.gridColumn);
+		gameStateVO.setMoney(mapSimulatorView.power);
+		gameStateVO.setHealth(mapSimulatorView.health);
+		gameStateVO.setGameValue(mapSimulatorView.gameValue);
+		gameStateVO.setWaveLevel(mapSimulatorView.waveLevel);
+		gameStateVO.setEnemiesOnMap(mapSimulatorView.enemiesOnMap);
+		gameStateVO.setMoveEnemy(mapSimulatorView.moveEnemy);
+
+		for (int i = 0; i < mapSimulatorView.gridRow; i++) {
+			for (int j = 0; j < mapSimulatorView.gridColumn; j++) {
+				char tileId = mapSimulatorView.gameValue[i][j];
+				if (tileId == 'R' || tileId == 'G' || tileId == 'B') {
+					Tower tower = getTowerObject(i, j);
+					int index = Integer.parseInt(Integer.toString(i)
+							+ Integer.toString(j));
+					gameStateVO.getTowerMap().put(index, tower);
+				}
+			}
+		}
+
+		JFileChooser fileChooser = new JFileChooser();
+		int saveValue = fileChooser.showSaveDialog(Game.getInstance());
+		if (saveValue == JFileChooser.APPROVE_OPTION) {
+			String fileName = fileChooser.getSelectedFile().getName();
+			String directory = fileChooser.getCurrentDirectory().toString();
+			String absolutePath = directory + "\\" + fileName + gameExtension;
+			try {
+				FileOutputStream fos = new FileOutputStream(absolutePath);
+				ObjectOutputStream oos = new ObjectOutputStream(fos);
+				oos.writeObject(mapSimulatorView); // write Tower object
+				oos.close();
+
+				XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(
+						new FileOutputStream(absolutePath)));
+				encoder.writeObject(gameStateVO);
+				encoder.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	public MapSimulatorView loadGameState(MapSimulatorView mapSimulatorView) {
+		GameStateVO gameStateVO = new GameStateVO();
+		JFileChooser fileChooser = new JFileChooser();
+		int openValue = fileChooser.showOpenDialog(Game.getInstance());
+		if (openValue == JFileChooser.APPROVE_OPTION) {
+			String filename = fileChooser.getSelectedFile().getName();
+			String directory = fileChooser.getCurrentDirectory().toString();
+			String absolutePath = directory + "\\" + filename;
+			try {
+
+				XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(
+						new FileInputStream(absolutePath)));
+				gameStateVO = (GameStateVO) decoder.readObject();
+				decoder.close();
+
+				mapSimulatorView.power = gameStateVO.getMoney();
+				mapSimulatorView.health = gameStateVO.getHealth();
+				mapSimulatorView.gridRow = gameStateVO.getGridRow();
+				mapSimulatorView.gridColumn = gameStateVO.getGridColumn();
+				mapSimulatorView.gameValue = gameStateVO.getGameValue();
+				mapSimulatorView.waveLevel = gameStateVO.getWaveLevel();
+				mapSimulatorView.enemiesOnMap = gameStateVO.getEnemiesOnMap();
+				mapSimulatorView.moveEnemy = gameStateVO.isMoveEnemy();
+
+				for (int i = 0; i < mapSimulatorView.gridRow; i++) {
+					for (int j = 0; j < mapSimulatorView.gridColumn; j++) {
+						char tileId = mapSimulatorView.gameValue[i][j];
+						if (tileId == 'R' || tileId == 'G' || tileId == 'B') {
+							int index = Integer.parseInt(Integer.toString(i)
+									+ Integer.toString(j));
+							Tower tower = gameStateVO.getTowerMap().get(index);
+							saveTowerObject(tower, i, j);
+						}
+					}
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		return mapSimulatorView;
 	}
 }
