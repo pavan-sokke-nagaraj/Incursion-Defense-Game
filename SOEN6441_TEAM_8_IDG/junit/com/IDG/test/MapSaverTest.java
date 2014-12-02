eeeb5b5a0ff965ae6cfbb401289a84df15d111e3
ajaykariappa <ajaykariappa@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-12-02 17:22:48 +0000

    Junit test for Map SAve and Load Functionality

---
+++ SOEN6441_TEAM_8_IDG/junit/com/IDG/test/MapSaverTest.java
@@ -0,0 +1,50 @@
+package com.IDG.test;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.IDG.mapBuilder.MapDetails;
+import com.IDG.utils.MapValidityHelper;
+
+public class MapSaverTest {
+
+MapDetails testSave = new MapDetails();
+
+
+@Before
+public void setUp() throws Exception{
+	
+	Calendar time = Calendar.getInstance();testSave.mapid=100;
+	testSave.columnSize=2;
+	testSave.rowSize=2;
+	testSave.creationTime=time.getTime();
+	testSave.gamestatus.add("GAME WON");
+	
+	testSave.writeToFile(testSave);
+	}
+	
+@Test
+public void testMapsave() {
+	
+	File file=new File("Resource/CustomMaps/GameMatrix/Map" + testSave.mapid + ".txt");
+	assertTrue(file.exists());
+}
+@Test
+public void testMapload()
+{
+	MapDetails temp=new MapDetails();
+	temp.mapid=100;
+temp=temp.readFromFile(temp);
+assertEquals(testSave.mapid,temp.mapid);
+assertEquals(testSave.columnSize,temp.columnSize);
+assertEquals(testSave.rowSize,temp.rowSize);
+assertEquals(testSave.gamestatus,temp.gamestatus);
+assertEquals(testSave.creationTime, temp.creationTime);
+}
+
+}
