135d9467cb209209d566a7ad3020d1460b3bec44
ajaykariappa <ajaykariappa@9907fc0b-72bc-3779-c841-5c6e322c1ea3>
2014-12-02 15:51:32 +0000

    Javadocs Updated

--- SOEN6441_TEAM_8_IDG/src/com/IDG/mapBuilder/MapDetails.java
+++ SOEN6441_TEAM_8_IDG/src/com/IDG/mapBuilder/MapDetails.java
@@ -18,119 +18,135 @@ import java.util.Map;
 import com.IDG.mapSimulator.MapSimulatorView;
 
 public class MapDetails implements Serializable {
-
+	/**
+	 * Variable to change format of Date/Time Saved
+	 */
 	public SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+	/**
+	 * Variable to change format of Date/Time Saved
+	 */
 	public DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-	
-	public Date creationTime =null;
+	/**
+	 * Variable to store creation date
+	 */
+	public Date creationTime = null;
+	/**
+	 * stores row size of Map
+	 */
 	public int rowSize;
+	/**
+	 * Stores Column Size of Map
+	 */
 	public int columnSize;
+	/**
+	 * ArrayList to store all time stamps when a Map is Modified
+	 */
 	public ArrayList<Date> modifiedTime = new ArrayList();
-	
+	/**
+	 * ArrayList to store all time stamps when a Map is played
+	 */
 	public ArrayList<Date> lastPlayedTime = new ArrayList();
-	
-	public  int mapid;
-	public StringBuffer mapdata=new StringBuffer();
-	public ArrayList<Integer> highscore=new ArrayList(5);
-    public ArrayList<String> gamestatus=new ArrayList();
-   public MapDetails()
-    {
-	   
-    }
-    
-    	
-   public void writeToFile (MapDetails e)
-   {
-	   if(e.mapid==955||e.mapid==608||e.mapid==167)
-	   {
-		   try
-	      {
-		   
-	         FileOutputStream fileOut=new FileOutputStream("Resource/DefaultMaps/GameMatrix/Map"+e.mapid+".txt");
-	         
-	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
-	         out.writeObject(e);
-	         out.close();
-	         fileOut.close();
-	         System.out.printf("Serialized data is saved in /Resource/DefaultMaps/GameMatrix/Map"+e.mapid+".txt");
-	      }catch(IOException i)
-	      {
-	          i.printStackTrace();
-	      }
+	/**
+	 * Stores Mapid - Part of filename to  identify the map file.
+	 */
+	public int mapid;
+	/**
+	 * Stores the MAP 
+	 */
+	public StringBuffer mapdata = new StringBuffer();
+	/**
+	 * Arraylist  of size 5 to store the High Score
+	 */
+	public ArrayList<Integer> highscore = new ArrayList(5);
+	/**
+	 * Stores the Game status /WON/LOST/TERMINATED 
+	 */
+	public ArrayList<String> gamestatus = new ArrayList();
 
-	   }
-	   
-	   else
-	   {
-		   try
-	      {
-		   
-	         FileOutputStream fileOut=new FileOutputStream("Resource/CustomMaps/GameMatrix/Map"+e.mapid+".txt");
-	         
-	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
-	         out.writeObject(e);
-	         out.close();
-	         fileOut.close();
-	         System.out.printf("Serialized data is saved in /Resource/CustomMaps/GameMatrix/Map"+e.mapid+".txt");
-	      }catch(IOException i)
-	      {
-	          i.printStackTrace();
-	      }
+	public MapDetails() {
 
-	   }
-	  }
-
-
-   public MapDetails readFromFile (MapDetails e1)
-   {
-	   MapDetails e=e1;
-	if(e.mapid==955||e.mapid==608||e.mapid==167)
-	{
-		 try
-		   {
-		      FileInputStream fileIn = new FileInputStream("Resource/DefaultMaps/GameMatrix/Map"+e.mapid+".txt");
-		      ObjectInputStream in = new ObjectInputStream(fileIn);
-		      e = (MapDetails) in.readObject();
-		      in.close();
-		      fileIn.close();
-		   }catch(IOException i)
-		   {
-		      i.printStackTrace();
-	
-		   }catch(ClassNotFoundException c)
-		   {
-		      System.out.println("Employee class not found");
-		      c.printStackTrace();
-		     
-		   }
 	}
-   
-	else
-	{
-		try
 	
-   {
-      FileInputStream fileIn = new FileInputStream("Resource/CustomMaps/GameMatrix/Map"+e.mapid+".txt");
-      ObjectInputStream in = new ObjectInputStream(fileIn);
-      e = (MapDetails) in.readObject();
-      in.close();
-      fileIn.close();
-   }catch(IOException i)
-   {
-      i.printStackTrace();
-     
-   }catch(ClassNotFoundException c)
-   {
-      
-      c.printStackTrace();
-     
-   }
+	/**
+	 * Function to Write Serialized object to File .
+	 */
+	public void writeToFile(MapDetails e) {
+		if (e.mapid == 955 || e.mapid == 608 || e.mapid == 167) {
+			try {
+				FileOutputStream fileOut = new FileOutputStream(
+						"Resource/DefaultMaps/GameMatrix/Map" + e.mapid
+								+ ".txt");
+				ObjectOutputStream out = new ObjectOutputStream(fileOut);
+				out.writeObject(e);
+				out.close();
+				fileOut.close();
+				System.out
+						.printf("Serialized data is saved in /Resource/DefaultMaps/GameMatrix/Map"
+								+ e.mapid + ".txt");
+			} catch (IOException i) {
+				i.printStackTrace();
+			}
+		} else {
+			try {
+				FileOutputStream fileOut = new FileOutputStream(
+						"Resource/CustomMaps/GameMatrix/Map" + e.mapid + ".txt");
+				ObjectOutputStream out = new ObjectOutputStream(fileOut);
+				out.writeObject(e);
+				out.close();
+				fileOut.close();
+				System.out
+						.printf("Serialized data is saved in /Resource/CustomMaps/GameMatrix/Map"
+								+ e.mapid + ".txt");
+			} catch (IOException i) {
+				i.printStackTrace();
+			}
+
+		}
 	}
-  
-   return e;
-   }
-   
-   
+
+	/**
+	 * Function to Read Serialized object from File .
+	 */
+	public MapDetails readFromFile(MapDetails e1) {
+		MapDetails e = e1;
+		if (e.mapid == 955 || e.mapid == 608 || e.mapid == 167) {
+			try {
+				FileInputStream fileIn = new FileInputStream(
+						"Resource/DefaultMaps/GameMatrix/Map" + e.mapid
+								+ ".txt");
+				ObjectInputStream in = new ObjectInputStream(fileIn);
+				e = (MapDetails) in.readObject();
+				in.close();
+				fileIn.close();
+			} catch (IOException i) {
+				i.printStackTrace();
+
+			} catch (ClassNotFoundException c) {
+				System.out.println("Employee class not found");
+				c.printStackTrace();
+
+			}
+		}
+
+		else {
+			try {
+				FileInputStream fileIn = new FileInputStream(
+						"Resource/CustomMaps/GameMatrix/Map" + e.mapid + ".txt");
+				ObjectInputStream in = new ObjectInputStream(fileIn);
+				e = (MapDetails) in.readObject();
+				in.close();
+				fileIn.close();
+			} catch (IOException i) {
+				i.printStackTrace();
+
+			} catch (ClassNotFoundException c) {
+
+				c.printStackTrace();
+
+			}
+		}
+
+		return e;
+	}
 
 }
-
